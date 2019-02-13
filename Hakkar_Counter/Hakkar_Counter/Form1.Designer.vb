dad5ea3da3d59f1a65dc8aaccac8b7814f7104d9
felixlepro <35881206+felixlepro@users.noreply.github.com>
2019-02-12 22:11:02 -0500

    Add files via upload

---
+++ Hakkar_Counter/Hakkar_Counter/Form1.Designer.vb
@@ -0,0 +1,177 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.nupBlood = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Pourcentage = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.nupTotal = New System.Windows.Forms.NumericUpDown()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.nupDraw = New System.Windows.Forms.NumericUpDown()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        CType(Me.nupBlood, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.nupTotal, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.nupDraw, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'nupBlood
+        '
+        Me.nupBlood.Location = New System.Drawing.Point(112, 41)
+        Me.nupBlood.Name = "nupBlood"
+        Me.nupBlood.Size = New System.Drawing.Size(98, 22)
+        Me.nupBlood.TabIndex = 0
+        Me.nupBlood.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(93, 17)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Blood in deck"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(257, 163)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(49, 17)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Health"
+        '
+        'Pourcentage
+        '
+        Me.Pourcentage.AutoSize = True
+        Me.Pourcentage.Location = New System.Drawing.Point(393, 163)
+        Me.Pourcentage.Name = "Pourcentage"
+        Me.Pourcentage.Size = New System.Drawing.Size(16, 17)
+        Me.Pourcentage.TabIndex = 9
+        Me.Pourcentage.Text = "0"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(12, 83)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(79, 17)
+        Me.Label10.TabIndex = 17
+        Me.Label10.Text = "Total cards"
+        '
+        'nupTotal
+        '
+        Me.nupTotal.Location = New System.Drawing.Point(112, 78)
+        Me.nupTotal.Name = "nupTotal"
+        Me.nupTotal.Size = New System.Drawing.Size(98, 22)
+        Me.nupTotal.TabIndex = 16
+        Me.nupTotal.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 121)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(47, 17)
+        Me.Label2.TabIndex = 19
+        Me.Label2.Text = "Draws"
+        '
+        'nupDraw
+        '
+        Me.nupDraw.Location = New System.Drawing.Point(112, 116)
+        Me.nupDraw.Name = "nupDraw"
+        Me.nupDraw.Size = New System.Drawing.Size(98, 22)
+        Me.nupDraw.TabIndex = 18
+        Me.nupDraw.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(114, 157)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(96, 23)
+        Me.Button1.TabIndex = 20
+        Me.Button1.Text = "Calculate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(260, 16)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(180, 132)
+        Me.ListBox1.TabIndex = 21
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(312, 161)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {6969, 0, 0, 0})
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(64, 22)
+        Me.NumericUpDown1.TabIndex = 22
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {30, 0, 0, 0})
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(463, 197)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.nupDraw)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.nupTotal)
+        Me.Controls.Add(Me.Pourcentage)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.nupBlood)
+        Me.Name = "Form1"
+        Me.Text = "Hakkar counter"
+        CType(Me.nupBlood, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.nupTotal, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.nupDraw, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents nupBlood As NumericUpDown
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Pourcentage As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents nupTotal As NumericUpDown
+    Friend WithEvents Label2 As Label
+    Friend WithEvents nupDraw As NumericUpDown
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+End Class
---
+++ Hakkar_Counter/Hakkar_Counter/Form1.vb
@@ -0,0 +1,145 @@
+﻿Public Class Form1
+    Dim bloodDeck As Integer
+    Dim otherCards As Integer
+    Dim totalcards As Integer
+    Dim draws As Integer
+    Dim maxDamage As Integer
+    Dim allChances As New List(Of Double)
+    Dim allDamage As New List(Of Integer)
+    Dim possibleDamage As New Dictionary(Of Integer, Double)
+
+    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles nupBlood.ValueChanged
+        bloodDeck = nupBlood.Value
+        If totalcards < bloodDeck Then
+            nupTotal.Value = bloodDeck
+        End If
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        bloodDeck = 0
+        otherCards = 0
+        totalcards = 0
+
+    End Sub
+
+
+    Private Sub NumericUpDown1_ValueChanged_1(sender As Object, e As EventArgs) Handles nupDraw.ValueChanged
+        draws = nupDraw.Value
+    End Sub
+
+    Private Sub nupTotal_ValueChanged(sender As Object, e As EventArgs) Handles nupTotal.ValueChanged
+        totalcards = nupTotal.Value
+        If totalcards < bloodDeck Then
+            nupBlood.Value = totalcards
+
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        allChances.Clear()
+        allDamage.Clear()
+
+
+        bloodDeck = nupBlood.Value
+        totalcards = nupTotal.Value
+        otherCards = totalcards - bloodDeck
+        maxDamage = 0
+
+        Draw(bloodDeck, bloodDeck, otherCards, 0, draws, 1)
+        ShowResults()
+        Lethal()
+
+
+    End Sub
+    Private Function Draw(ByVal blood As Integer, ByVal bloodMax As Integer, ByVal other As Integer, ByVal damage As Integer, drawsLeft As Integer, chance As Double) As Integer
+
+        If other > 0 And drawsLeft > 0 Then
+            DrawOther(blood, bloodMax, other, damage, drawsLeft, chance)
+        Else
+            damage += OnlyBlood(blood, drawsLeft)
+            allChances.Add(chance)
+            allDamage.Add(damage)
+            Return 0
+        End If
+        If blood > 0 And drawsLeft > 0 Then
+            DrawBlood(blood, bloodMax, other, damage, drawsLeft, chance)
+        End If
+        Return 0
+    End Function
+
+    Private Function DrawBlood(ByVal blood As Integer, ByVal bloodMax As Integer, ByVal other As Integer, ByVal damage As Integer, drawsLeft As Integer, chance As Double) As Integer
+        chance *= blood
+        chance /= (other + blood)
+        blood -= 1
+        damage += 3
+        Draw(blood, bloodMax, other, damage, drawsLeft, chance)
+
+        Return 0
+    End Function
+
+    Private Function DrawOther(ByVal blood As Integer, ByVal bloodMax As Integer, ByVal other As Integer, ByVal damage As Integer, drawsLeft As Integer, chance As Double) As Integer
+        chance *= other
+        chance /= (other + blood)
+        other -= 1
+        drawsLeft -= 1
+        bloodMax += (bloodMax - blood)
+        blood = bloodMax
+        Draw(blood, bloodMax, other, damage, drawsLeft, chance)
+        Return 0
+    End Function
+    Private Function OnlyBlood(blood As Integer, drawsLeft As Integer) As Integer
+        Dim damageBlood As Integer = 0
+        Dim fatigue = 1
+        For i As Integer = 1 To drawsLeft
+            damageBlood += 3 * blood
+            damageBlood += fatigue
+            fatigue += 1
+
+            blood *= 2
+        Next
+
+        Return damageBlood
+    End Function
+    Private Function ShowResults() As Integer
+        Dim index As Integer
+        possibleDamage.Clear()
+
+        For i As Integer = 0 To allDamage.Last
+            index = allDamage.FindIndex(Function(p) p = i)
+            While index <> -1
+                Try
+                    possibleDamage.Item(i) += allChances.Item(index)
+                Catch ex As Exception
+                    possibleDamage.Add(i, allChances.Item(index))
+                End Try
+                allDamage.RemoveAt(index)
+                allChances.RemoveAt(index)
+                index = allDamage.FindIndex(Function(p) p = i)
+            End While
+        Next
+        ListBox1.Items.Clear()
+
+        For Each pair In possibleDamage
+            ListBox1.Items.Add(pair.Key.ToString + vbTab + (pair.Value * 100).ToString("0.00") + "%")
+        Next
+
+        Return 0
+    End Function
+
+    Private Function Lethal()
+        Dim p As Double = 0
+        For Each pair In possibleDamage
+            If pair.Key >= NumericUpDown1.Value Then
+                p += pair.Value * 100
+
+            End If
+        Next
+        Pourcentage.Text = p.ToString("0.00") + "%"
+    End Function
+    Private Sub NumericUpDown1_ValueChanged_2(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged
+        Lethal()
+
+    End Sub
+
+End Class
